055699313ddab5e4fe83c1caea1b55aeb93fdb4d
john <john@jpatmore.plus.com>
2021-09-05 21:33:02 +0100

    Turtle shapes cleaned up

--- turtle_shapes.py
+++ turtle_shapes.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 
 colors = ["silver", "red", "yellow", "green", "blue", "cyan", "white",]
@@ -19,21 +20,30 @@ _shapes = {
     "triangle": ((10, -5.77), (0, 11.55),
                  (-10, -5.77)),
     "classic": ((0, 0), (-5, -9), (0, -7), (5, -9)),
+    "fighter": ((0, 5), (-5, 0), (-10, 10), (-5, -10), (0, 0), (5, -10), (10, 10), (5, 0), (0, 5))
 }
 
 screen = Screen()
 screen.setup(width=700, height=400)
 screen.bgcolor("black")
+turtle.addshape('fighter', _shapes['fighter'])
+
+# turtle.register_shape('my_shape', ((0, 5), (-5, 0), (-10, 10), (-5, -10), (5, -10), (10, 10), (5, 0), (0, 5)))
+# tim = Turtle(shape='my_shape')
+
 i = 0
 for shape in _shapes:
     tim = Turtle(shape=shape)
     tim.color(colors[i])
+    tim.speed(1)
     i += 1
+    tim.pu()
     for item in _shapes[shape]:
         # print(f'{item = }')
         x = item[0] * 10
         y = item[1] * 10
         tim.goto(x, y)
+        tim.pd()
     tim.goto(_shapes[shape][0][0] * 10, _shapes[shape][0][1] * 10)
     tim.hideturtle()
 screen.exitonclick()
